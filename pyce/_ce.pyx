dfe8ed5aab5e55e98523bcee2224ff17d5f84f14
synodriver <diguohuangjiajinweijun@gmail.com>
2022-07-20 21:27:51 +0800

    Update _ce.pyx

--- pyce/_ce.pyx
+++ pyce/_ce.pyx
@@ -1,17 +1,18 @@
 # cython: language_level=3
 # cython: cdivision=True
 # distutils: language=c++
-from pyce.ce cimport TToolhelp as TToolhelp_C, DWORD, bool
+import cython
+from pyce cimport ce
 
-
+@cython.final
 cdef class TToolhelp:
-    cdef TToolhelp_C* ins
+    cdef ce.TToolhelp* ins
 
-    def __cinit__(self, DWORD dwFlags = 0, DWORD dwProcessID = 0):
-        self.ins = new TToolhelp_C(dwFlags,dwProcessID)
+    def __cinit__(self, ce.DWORD dwFlags = 0, ce.DWORD dwProcessID = 0):
+        self.ins = new ce.TToolhelp(dwFlags,dwProcessID)
 
     def __dealloc__(self):
         del self.ins
 
-    cpdef inline bool CreateSnapshot(self, DWORD dwFlags, DWORD dwProcessID = 0):
+    cpdef inline ce.bool CreateSnapshot(self, ce.DWORD dwFlags, ce.DWORD dwProcessID = 0):
         return self.ins.CreateSnapshot( dwFlags, dwProcessID )
